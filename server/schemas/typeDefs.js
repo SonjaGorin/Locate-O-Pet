e4b0f7abeda700b6e9ba8c04a67d560c5d77012f
leviathan <jake66martin@hotmail.com>
2024-01-22 14:57:21 -0500

    working on typedef file

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,50 @@
+const typeDefs = `
+
+type Owner {
+    _id: ID,
+    userName: String,
+    name: String,
+    email: String,
+    password: String,
+    address: String,
+    phoneNumber: String,
+    petsLost: [Pet]
+}
+
+type User {
+    _id: ID,
+    username: String,
+    name: String, 
+    email: String,
+    password: String,
+    phoneNumber: String,
+    petsSeen: [pet]
+}
+
+type Pet {
+    _id: ID,
+    species: String,
+    sex: String,
+    breed: String,
+    colours: [String]
+}
+
+type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+}
+
+type Mutation {
+    addUser(userName: String!, name: String!, email: String!, password: String!, phoneNumber: String!): Auth
+    addOwner(username: String!, name: String!, email: String!, password: String!, address: String!, phoneNumber: String!): Auth
+
+    login(email: String!, password: String!): Auth
+}
+
+`;
+
+module.exports = typeDefs;
