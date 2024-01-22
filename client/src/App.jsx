de94109168004e610b7de83bc35a488ace74e02b
Sonja <sonjaljatkovic@gmail.com>
2024-01-22 14:38:37 -0500

    fixed issues with setting up the variables, zoom now works

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useRef, useEffect, useState } from 'react';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import mapboxgl from '!mapbox-gl';
  
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ3NvbmphIiwiYSI6ImNscm9kZ3RheDFoMGoybG9mZGZiNGphOG4ifQ.xYb4Ch19HGpuJpK2BXQ3tg';
 
@@ -8,9 +8,9 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZ3NvbmphIiwiYSI6ImNscm9kZ3RheDFoMGoybG9mZGZiN
 export default function App() {
     const mapContainer = useRef(null);
     const map = useRef(null);
-    const [lng, setLng] = useState(-75.695);
-    const [lat, setLat] = useState(45.4215);
-    const [zoom, setZoom] = useState(9);
+    const [lng] = useState(-75.695);
+    const [lat] = useState(45.4215);
+    const [zoom] = useState(9);
 
     useEffect(() => {
         if (map.current) return; // initialize map only once
