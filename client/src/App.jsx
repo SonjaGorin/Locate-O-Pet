d1fa3cad374486af8b4886b4153345a6a7104821
Sonja <sonjaljatkovic@gmail.com>
2024-01-22 12:02:52 -0500

    fixed the issues ang got the map to render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ3NvbmphIiwiYSI6ImNscm9kZ3RheDFoMGoybG9mZGZiNGphOG4ifQ.xYb4Ch19HGpuJpK2BXQ3tg';
+
 // import './App.css'
 
+export default function App() {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-75.695);
+    const [lat, setLat] = useState(45.4215);
+    const [zoom, setZoom] = useState(9);
 
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [lng, lat],
+            zoom: zoom
+        });
+    });
 
-export default App
+    return (
+        <div>
+        <div ref={mapContainer} className="map-container" />
+        </div>
+    );
+}
