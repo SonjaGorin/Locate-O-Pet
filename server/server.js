abe8b4b25f8ee967fd80a02fec6280e6f1d69540
leviathan <jake66martin@hotmail.com>
2024-01-22 08:41:38 -0500

    installed required dependencies

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
+const { ApolloServer } = require('@apollo/server');
+const { authMiddleware } = require('./utils/auth');
+
+
