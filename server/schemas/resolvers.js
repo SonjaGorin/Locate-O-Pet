9901d868ae85dd02f6752a26ad476aaf184824f0
leviathan <jake66martin@hotmail.com>
2024-01-22 15:47:02 -0500

    started resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,13 @@
+const { User, Owner } = require("../models/index");
+const { signToken, AuthenticationError } = require("../utils/auth");
+
+const resolvers = {
+    Query: {
+
+    },
+    Mutations: {
+
+    }
+}
+
+module.exports = resolvers;
