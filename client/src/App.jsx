87bfa79a789c127813fc523d59066385d61bda0f
Gus Miller <gustavo.miller@miller-hs.com>
2024-01-22 21:52:44 -0500

    Merge conflict with Sonja - resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -44,27 +44,16 @@ const authLink = setContext((_, { headers }) => {
 const client = new ApolloClient({ link: authLink.concat(httpLink), cache: new InMemoryCache(), });
 
 export default function App() {
-     const mapContainer = useRef(null);
-     const map = useRef(null);
-     const [lng] = useState(-75.695);
-     const [lat] = useState(45.4215);
-     const [zoom] = useState(9);
-
-     useEffect(() => {
-          if (map.current) return; // initialize map only once
-          map.current = new mapboxgl.Map({
-               container: mapContainer.current,
-               style: 'mapbox://styles/mapbox/streets-v12',
-               center: [lng, lat],
-               zoom: zoom
-          });
-     });
-
      return (
           <ApolloProvider client={client}>
                <NavigationBar />
                <Outlet />
-               <div ref={mapContainer} className="map-container" />
+               <div>
+                    <div className="sidebar">
+                         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+                    </div>
+                    <div ref={mapContainer} className="map-container" />
+               </div>
           </ApolloProvider>
      );
 }
