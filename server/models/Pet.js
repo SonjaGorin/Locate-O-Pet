04388dbc845cb4297e208c5bb5387dc8cc77c321
Jake66Martin <139023505+Jake66Martin@users.noreply.github.com>
2024-01-23 13:02:50 -0500

    Update Pet.js

--- server/models/Pet.js
+++ server/models/Pet.js
@@ -11,6 +11,10 @@ const petSchema = new Schema({
     type: String,
     required: true,
   },
+  createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
   colours: [
     {
     type: String,
