caae1161a662cc25e3a4d117e02eb676810b02a9
Jake66Martin <139023505+Jake66Martin@users.noreply.github.com>
2024-01-23 13:03:42 -0500

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -33,6 +33,10 @@ const userSchema = new Schema(
       type: String,
       required: true
     },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
     petsSeen: [petSchema]
   },
   {
