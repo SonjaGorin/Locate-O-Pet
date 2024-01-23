b17f4827a6c216c76ff80f0f0f522a65465a38a2
Gus Miller <gustavo.miller@miller-hs.com>
2024-01-22 21:21:58 -0500

    Initial implementation Navbar and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,70 @@
+/*******************************************************************
+ * Carleton Bootcamp - 2024
+ * Copyright 2024 Sonja Gorin, Jacob Martin, Gustavo Miller
+ * License: free and unencumbered software
+ * Assignment # 20 - React Portfolio
+ * 
+ * Filename: app.jsx
+ * Date : 1/22/2024 9:05:01 PM
+ *******************************************************************/
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import NavigationBar from './components/Navigator/index';
+import { setContext } from '@apollo/client/link/context';
+
 import { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl';
- 
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ3NvbmphIiwiYSI6ImNscm9kZ3RheDFoMGoybG9mZGZiNGphOG4ifQ.xYb4Ch19HGpuJpK2BXQ3tg';
 
-// import './App.css'
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({ uri: '/graphql', });
+
+/**
+ * Construct request middleware that will attach the JWT token to 
+ * every request as an `authorization` header. It first gets the 
+ * authentication token from local storage if it exists. It then 
+ * return the headers to the context so httpLink can read them
+ */
+const authLink = setContext((_, { headers }) => {
+     const token = localStorage.getItem('id_token');
+     return {
+          headers: { ...headers, authorization: token ? `Bearer ${token}` : '', },
+     };
+});
+
+/**
+ * There is only one endpoint that ends with Slash Graphql. Apollo Client stores
+ * the results of your GraphQL queries in a local, normalized, in-memory cache. 
+ * This enables Apollo Client to respond almost immediately to queries for 
+ * already-cached data, without even sending a network request.
+ * https://www.apollographql.com/docs/react/caching/overview
+ */
+const client = new ApolloClient({ link: authLink.concat(httpLink), cache: new InMemoryCache(), });
 
 export default function App() {
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng] = useState(-75.695);
-    const [lat] = useState(45.4215);
-    const [zoom] = useState(9);
+     const mapContainer = useRef(null);
+     const map = useRef(null);
+     const [lng] = useState(-75.695);
+     const [lat] = useState(45.4215);
+     const [zoom] = useState(9);
 
-    useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v12',
-            center: [lng, lat],
-            zoom: zoom
-        });
-    });
+     useEffect(() => {
+          if (map.current) return; // initialize map only once
+          map.current = new mapboxgl.Map({
+               container: mapContainer.current,
+               style: 'mapbox://styles/mapbox/streets-v12',
+               center: [lng, lat],
+               zoom: zoom
+          });
+     });
 
-    return (
-        <div>
-        <div ref={mapContainer} className="map-container" />
-        </div>
-    );
+     return (
+          <ApolloProvider client={client}>
+               <NavigationBar />
+               <Outlet />
+               <div ref={mapContainer} className="map-container" />
+          </ApolloProvider>
+     );
 }
---
+++ client/src/components/Navigator/navigation.jsx
@@ -0,0 +1,69 @@
+/*******************************************************************
+ * Carleton Bootcamp - 2024
+ * Copyright 2024 Sonja Gorin, Jacob Martin, Gustavo Miller
+ * License: free and unencumbered software
+ * Assignment # 20 - React Portfolio
+ * 
+ * Filename: Navigation.jsx
+ * Date : 1/14/2024 17:03:48 PM
+ *******************************************************************/
+import { Link, useLocation } from 'react-router-dom';
+import Auth from "../../utils/auth";
+
+function Navigation() {
+
+     const cp = useLocation().pathname;
+
+     /**
+      * This function will build the options for when user is logged in and for when they are not. All options are 
+      * available for anyone that uses the website
+      * @returns 
+      */
+     function showOptions() {
+          if (Auth.loggedIn()) {
+               return (
+                    <ul className="navbar-nav" id="navScrollspy">
+                         <li key={1} className="nav-item"><Link className={`nav-link fs-3 ${cp === '/' ? 'active' : ''}`} to="/" >Map</Link></li>
+                         <li key={2} className="nav-item"><Link className={`nav-link fs-3 ${cp === '/About' ? 'active' : ''}`} to="/About" >About Us</Link></li>
+                         <li key={3} className="nav-item"><Link className={`nav-link fs-3 ${cp === '/LostPets' ? 'active' : ''}`} to="/LostPets" >Lost Pets</Link></li>
+                         <li key={4} className="nav-item"><Link className={`nav-link fs-3 ${cp === '/Spotted' ? 'active' : ''}`} to="/Spotted" >Spotted Pets</Link></li>
+                         <li key={5} className="nav-item"><Link className={`nav-link fs-3 ${cp === '/Post' ? 'active' : ''}`} to="/Post" >Post</Link></li>
+                         <li key={6} className="nav-item"><Link className={`nav-link fs-3 ${cp === '/Contact' ? 'active' : ''}`} to="/Contact" >Contact Us</Link></li>
+                         <li key={7} className="nav-item"><Link className={`nav-link fs-3 ${cp === '/Logout' ? 'active' : ''}`} to="/Logout" >Logout</Link></li>
+                    </ul>
+               )
+          } else {
+               return (
+                    <ul className="navbar-nav" id="navScrollspy">
+                         <li key={1} className="nav-item"><Link className={`nav-link fs-3 ${cp === '/' ? 'active' : ''}`} to="/" >Map</Link></li>
+                         <li key={2} className="nav-item"><Link className={`nav-link fs-3 ${cp === '/About' ? 'active' : ''}`} to="/About" >About Us</Link></li>
+                         <li key={3} className="nav-item"><Link className={`nav-link fs-3 ${cp === '/LostPets' ? 'active' : ''}`} to="/LostPets" >Lost Pets</Link></li>
+                         <li key={4} className="nav-item"><Link className={`nav-link fs-3 ${cp === '/Spotted' ? 'active' : ''}`} to="/Spotted" >Spotted Pets</Link></li>
+                         <li key={5} className="nav-item"><Link className={`nav-link fs-3 ${cp === '/Post' ? 'active' : ''}`} to="/Post" >Post</Link></li>
+                         <li key={6} className="nav-item"><Link className={`nav-link fs-3 ${cp === '/Contact' ? 'active' : ''}`} to="/Contact" >Contact Us</Link></li>
+                         <li key={7} className="nav-item"><Link className={`nav-link fs-3 ${cp === '/Login' ? 'active' : ''}`} to="/Login" >Login</Link></li>
+                    </ul>
+               )
+          }
+     }
+
+     return (
+
+          <nav className="navbar bg-primary navbar-dark navbar-expand-lg">
+               <div className="container">
+                    <Link className="navbar-brand" to="/"><i className="fa fa-smile-o" aria-hidden="true"></i> Locate-o-Pet</Link>
+                    <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                         <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                         <div>
+                              {showOptions()}
+                         </div>
+                    </div>
+               </div>
+          </nav >
+     );
+}
+
+export default Navigation;
