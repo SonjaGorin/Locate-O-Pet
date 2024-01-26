28a0599e9d712d472bf45fd1ffa71a2dbf410b0a
Sonja <sonjaljatkovic@gmail.com>
2024-01-25 23:32:25 -0500

    added color to pet info cards

--- client/src/components/PetPostCard/PetPostCard.jsx
+++ client/src/components/PetPostCard/PetPostCard.jsx
@@ -3,13 +3,16 @@ import "./PetPostCard.css"
 export default function PetPostCard(pet) {
     if (pet.status === "isLost") {
         var speciesText = "This is my lost";
+        var subclass = "background-red"
     } else if (pet.status === "isSeen") {
         var speciesText = "I saw a";
+        var subclass = "background-green"
     } else {
         throw new Error(`Unexpected pet status ${pet.status}`)
     }
+    const className = `pet-post-card ${subclass}`
     return (
-        <div className="pet-post-card">
+    <div className={className}>
             <h2 className="card-text">{speciesText} {pet.species}</h2>
             <h2 className="card-text">Breed: {pet.breed}</h2>
             <h2 className="card-text">Colour: {pet.colours}</h2>
