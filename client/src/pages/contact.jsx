5b69317d35b165d43d2304b7c5552f51ee7566ac
leviathan <jake66martin@hotmail.com>
2024-01-27 12:52:00 -0500

    added useState to populate fields

--- client/src/pages/contact.jsx
+++ client/src/pages/contact.jsx
@@ -13,8 +13,16 @@ import GraphEnv from '../components/Contact/envelope.jsx';
 import GraphPhone from '../components/Contact/phoneicon.jsx';
 import { Container, Row, Form } from 'react-bootstrap';
 import Swal from 'sweetalert2';
+import { useState} from "react"
 
 export default function Contact() {
+     const [formState, setFormState] = useState({fullname: "", email: "", phone: "", message: "" });
+
+     const handleChange = (event) => {
+          const { name, value } = event.target;
+          setFormState({ ...formState, [name]: value });
+        };
+
 
      const handleClick = () => {
           Swal.fire({
@@ -41,26 +49,26 @@ export default function Contact() {
                                    <Form action="#!">
                                         <Row className="gy-4 gy-xl-5 p-4 p-xl-5">
                                              <div className="col-12">
-                                                  <label htmlfor="fullname" className="form-label">Full Name <span className="text-danger">*</span></label>
-                                                  <input type="text" className="form-control" id="fullname" name="fullname" value="" required />
+                                                  <label htmlFor="fullname" className="form-label">Full Name <span className="text-danger">*</span></label>
+                                                  <input type="text" className="form-control" id="fullname" name="fullname" onChange={handleChange} required />
                                              </div>
                                              <div className="col-12 col-md-6">
-                                                  <label htmlfor="email" className="form-label">Email <span className="text-danger">*</span></label>
+                                                  <label htmlFor="email" className="form-label">Email <span className="text-danger">*</span></label>
                                                   <div className="input-group">
                                                        <span className="input-group-text"><GraphEnv /></span>
-                                                       <input type="email" className="form-control" id="email" name="email" value="" required />
+                                                       <input type="email" className="form-control" id="email" name="email" onChange={handleChange} required />
                                                   </div>
                                              </div>
                                              <div className="col-12 col-md-6">
-                                                  <label htmlfor="phone" className="form-label">Phone Number</label>
+                                                  <label htmlFor="phone" className="form-label">Phone Number</label>
                                                   <div className="input-group">
                                                        <span className="input-group-text"><GraphPhone /></span>
-                                                       <input type="tel" className="form-control" id="phone" name="phone" value="" />
+                                                       <input type="tel" className="form-control" id="phone" name="phone" onChange={handleChange}/>
                                                   </div>
                                              </div>
                                              <div className="col-12">
-                                                  <label htmlfor="message" className="form-label">Message <span className="text-danger">*</span></label>
-                                                  <textarea className="form-control" id="message" name="message" rows="3" required></textarea>
+                                                  <label htmlFor="message" className="form-label">Message <span className="text-danger">*</span></label>
+                                                  <textarea className="form-control" id="message" name="message" rows="3" onChange={handleChange} required></textarea>
                                              </div>
                                              <div className="col-12">
                                                   <div className="d-grid">
