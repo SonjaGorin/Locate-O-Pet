8a8849bc53d98ffb62865465fd275122567d8298
leviathan <jake66martin@hotmail.com>
2024-01-26 23:28:38 -0500

    fixed footer issues

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer() {
+  
+    return (
+      <footer className="bg-primary d-flex justify-content-center align-items-center text-white footer-height">
+      Carleton University Coding Bootcamp © Copyright 2024
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
