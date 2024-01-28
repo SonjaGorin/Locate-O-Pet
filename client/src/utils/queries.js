d1f4b511e3d930c35e70d7953d00c709595883a5
leviathan <jake66martin@hotmail.com>
2024-01-28 13:25:58 -0500

    implemented email duplicate validation

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,88 +1,93 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const QUERY_ME = gql`
-query Query {
-  me {
-    _id
-    name
-    email
-    password
-    role
-    petsSeen {
+  query Query {
+    me {
       _id
-      species
-      sex
-      breed
-      createdAt
-      colours
-      message
-      status
-      img
-      lng
-      lat
-    }
-    petsLost {
-      _id
-      species
-      sex
-      breed
-      createdAt
-      colours
-      message
-      status
-      img
-      lng
-      lat
+      name
+      email
+      password
+      role
+      petsSeen {
+        _id
+        species
+        sex
+        breed
+        createdAt
+        colours
+        message
+        status
+        img
+        lng
+        lat
+      }
+      petsLost {
+        _id
+        species
+        sex
+        breed
+        createdAt
+        colours
+        message
+        status
+        img
+        lng
+        lat
+      }
     }
   }
-}
-`
+`;
 
 export const QUERY_PETID = gql`
-query Query($id: ID!) {
-  petById(_id: $id) {
-    _id
-    species
-    sex
-    breed
-    createdAt
-    colours
-    message
-    status
-    img
-    lng
-    lat
-    user {
+  query Query($id: ID!) {
+    petById(_id: $id) {
       _id
-      name
-      email
-      password
+      species
+      sex
+      breed
+      createdAt
+      colours
+      message
+      status
+      img
+      lng
+      lat
+      user {
+        _id
+        name
+        email
+        password
+      }
     }
   }
-}
-
-`
+`;
 
 export const QUERY_ALLPETS = gql`
-query AllPets {
-  allPets {
-    _id
-    species
-    sex
-    breed
-    createdAt
-    colours
-    message
-    status
-    img
-    lng
-    lat
-    user {
+  query AllPets {
+    allPets {
       _id
-      name
-      email
-      password
+      species
+      sex
+      breed
+      createdAt
+      colours
+      message
+      status
+      img
+      lng
+      lat
+      user {
+        _id
+        name
+        email
+        password
+      }
     }
   }
-}
-`
+`;
+
+export const USER_EMAIL = gql`
+  query UserEmail($email: String!) {
+    userEmail(email: $email) 
+  }
+`;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,7 +9,7 @@ const resolvers = {
       }
 
       const user = User.findOne({ _id: context.user._id });
-      return user
+      return user;
     },
 
     petById: async (parent, { _id }, context) => {
@@ -21,23 +21,44 @@ const resolvers = {
       const pets = await Pet.find().populate("user");
       return pets;
     },
+
+    userEmail: async (parent, { email }, context) => {
+      try {
+        const user = await User.findOne({ email });
+        return user !== null
+      } catch (error) {
+        console.error("Error while checking duplicate email:", error);
+        throw error;
+      }
+    },
   },
   Mutation: {
-    addUser: async (parent, { name, email, password, phoneNumber }) => {
-      console.log({
-        name,
-        email,
-        password,
-        phoneNumber,
-      });
-      const user = await User.create({
-        name,
-        email,
-        password,
-        phoneNumber,
-      });
-      const token = signToken(user);
-      return { token, user };
+    addUser: async (parent, { name, email, password }) => {
+      try {
+        const isDuplicateEmail = await User.findOne({ email });
+
+        if (isDuplicateEmail) {
+          throw new Error(
+            "Email already exists. Please choose a different email."
+          );
+        }
+
+        console.log({
+          name,
+          email,
+          password
+        });
+        const user = await User.create({
+          name,
+          email,
+          password
+        });
+        const token = signToken(user);
+        return { token, user };
+      } catch (error) {
+        console.error(error);
+        // throw new Error("An error occurred during user creation.");
+      }
     },
 
     login: async (parent, { email, password }) => {
@@ -58,7 +79,8 @@ const resolvers = {
     },
 
     addSeenPet: async (parent, { input }, context) => {
-      const { species, sex, breed, colours, message, status, img, lng, lat } = input;
+      const { species, sex, breed, colours, message, status, img, lng, lat } =
+        input;
 
       if (!context.user) {
         throw AuthenticationError;
@@ -91,7 +113,8 @@ const resolvers = {
     },
 
     addLostPet: async (parent, { input }, context) => {
-            const { species, sex, breed, colours, message, status, img, lng, lat } = input;
+      const { species, sex, breed, colours, message, status, img, lng, lat } =
+        input;
 
       if (!context.user) {
         throw AuthenticationError;
@@ -130,32 +153,29 @@ const resolvers = {
 
       const user = await User.findOne({ _id: context.user._id });
 
-       if (user.petsSeen.includes(_id)) {
+      if (user.petsSeen.includes(_id)) {
+        const petRemoved = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { petsSeen: _id } },
+          { new: true }
+        );
+        const petDeleted = await Pet.findOneAndDelete({
+          _id: _id,
+        });
 
-      const petRemoved = await User.findOneAndUpdate(
-        { _id: context.user._id },
-        { $pull: { petsSeen: _id } },
-        { new: true }
-      );
-      const petDeleted = await Pet.findOneAndDelete({
-        _id: _id,
-      });
+        return petDeleted;
+      } else {
+        const petRemoved = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { petsLost: _id } },
+          { new: true }
+        );
+        const petDeleted = await Pet.findOneAndDelete({
+          _id: _id,
+        });
 
-      return petDeleted;
-    }
-    else {
-
-      const petRemoved = await User.findOneAndUpdate(
-        { _id: context.user._id },
-        { $pull: { petsLost: _id } },
-        { new: true }
-      );
-      const petDeleted = await Pet.findOneAndDelete({
-        _id: _id,
-      });
-
-      return petDeleted;
-    }
+        return petDeleted;
+      }
     },
   },
 };
