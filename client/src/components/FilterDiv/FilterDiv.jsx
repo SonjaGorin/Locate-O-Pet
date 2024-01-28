a3795f6a577b422b8d74adcd5f1d8b5b7c7e6c05
Sonja <sonjaljatkovic@gmail.com>
2024-01-28 02:56:09 -0500

    started filters and added user location

--- client/src/components/FilterDiv/FilterDiv.jsx
+++ client/src/components/FilterDiv/FilterDiv.jsx
@@ -4,10 +4,34 @@ export default function FilterDiv({open}) {
     if (!open) {
         return (<div></div>);
     }
-    
+
     return (
         <div className="filter-div">
-            <h1>hello</h1>
+            <h1 className="filters-title">Filters</h1>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" />
+                <label class="form-check-label" for="exampleRadios1">
+                    Show all pets
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" />
+                <label class="form-check-label" for="exampleRadios2">
+                    Show only lost pets
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3" />
+                <label class="form-check-label" for="exampleRadios3">
+                    Show only seen pets
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" />
+                <label class="form-check-label" for="exampleRadios1">
+                    Show my posts
+                </label>
+            </div>
         </div>
     )
 }
