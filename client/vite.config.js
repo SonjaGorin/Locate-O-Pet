b2f2ecd9e43a55b826ad77f58ca446392338ec4f
leviathan <jake66martin@hotmail.com>
2024-01-30 16:42:02 -0500

    made chunking into smaller size

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
  
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 500 * 1024
+  },
   server: {
     port: 3000,
     open: true,
